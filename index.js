a29912551e9915da819fe645d47511eec49435e8
Marcel Schoombee <marcelsch01@yahoo.com>
2024-04-19 08:36:55 +0200

    Filter tasks bug fixed

--- index.js
+++ index.js
@@ -86,22 +86,22 @@ function filterAndDisplayTasksByBoard(boardName) {
     const tasksContainer = document.createElement("div");
     column.appendChild(tasksContainer);
 
-    filteredTasks.filter(task => task.status = status).forEach(task => { 
+    filteredTasks.filter(task => task.status === status).forEach(task => { 
       const taskElement = document.createElement("div");
       taskElement.classList.add("task-div");
-      taskElement.textContent = task.title;
+      taskElement.textContent = (task.title);
       taskElement.setAttribute('data-task-id', task.id);
 
       // Listen for a click event on each task and open a modal
-      taskElement.click() => { 
+      taskElement.addEventListener('click', () => { 
         openEditTaskModal(task);
       });
 
       tasksContainer.appendChild(taskElement);
     });
   });
-}
 
+};
 
 function refreshTasksUI() {
   filterAndDisplayTasksByBoard(activeBoard);
